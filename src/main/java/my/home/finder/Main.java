ab0208f4a9d2560a007ce265f69c42165032273a
Максим Каримов <karimov@company.dnevnik.ru>
2016-02-11 12:46:23 +0300

    initial commit

---
+++ src/main/java/my/home/finder/Main.java
@@ -0,0 +1,17 @@
+package my.home.finder;
+
+
+import org.bytedeco.javacv.Frame;
+
+/**
+ * Created by karimov on 11.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    private void analyse() {
+        Frame frame =  new Frame();
+    }
+}
